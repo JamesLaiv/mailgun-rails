19084c158535cd23f1679351bd814133a5609689
Pavel <pavel.evst@gmail.com>
2014-07-23 18:10:19 +0700

    Add support for http proxy, clean up syntax

--- lib/mailgun-rails.rb
+++ lib/mailgun-rails.rb
@@ -24,7 +24,6 @@ module Mailgun
     end
 
     def deliver!(mail)
-
       body              = Curl::PostField.content("message", mail.encoded)
       body.remote_file  = "message"
       body.content_type = "application/octet-stream"
@@ -36,13 +35,15 @@ module Mailgun
         data << Curl::PostField.content("to", destination)
       end
 
-      curl                     = Curl::Easy.new("https://api:#{self.api_key}@api.mailgun.net/v2/#{self.api_host}/messages.mime")
+      curl = Curl::Easy.new("https://api:#{self.api_key}@api.mailgun.net/v2/#{self.api_host}/messages.mime")
+      if ENV['http_proxy']
+        curl.proxy_url = ENV['http_proxy']
+      end
       curl.ssl_verify_peer = false
       curl.multipart_form_post = true
       curl.http_post(*data)
 
       if curl.response_code != 200
-
         begin
           error = ActiveSupport::JSON.decode(curl.body_str)["message"]
         rescue
@@ -51,13 +52,9 @@ module Mailgun
 
         raise Mailgun::DeliveryError.new(error)
       end
-
     end
 
-
-  end
-
-end
+  end # Mailgun::DeliveryMethod
+end # Mailgun
 
 ActionMailer::Base.add_delivery_method :mailgun, Mailgun::DeliveryMethod
-
